e35f889fd6ee26cb6e222ab07257a8be98918c71
Leonardo Ramírez <leonardon473@gmail.com>
2022-01-06 23:24:21 -0600

    added: 8.4 Using Gamma Correction for Accurate Color Intensity is added.

--- cmd/main.go
+++ cmd/main.go
@@ -51,7 +51,7 @@ func rayColor(ray r.Ray, world r.Hittable, depth int) r.Color {
 	if depth <= 0 {
 		return r.Color{}
 	}
-	if world.Hit(ray, 0, r.Infinity, &rec) {
+	if world.Hit(ray, 0.001, r.Infinity, &rec) {
 		target := rec.Point.Add(rec.Normal).Add(r.RandomInUnitSphere())
 		return rayColor(
 			r.Ray{Origin: rec.Point, Direction: target.Sub(rec.Point)},
